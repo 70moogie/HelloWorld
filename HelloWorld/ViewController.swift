33c313eb1fdc4c35632800c54e1abe9debe0db94
70moogie <70moogie@gmail.com>
2020-06-02 12:57:42 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by St_NY on 2020/06/02.
+//  Copyright © 2020 70moogie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
